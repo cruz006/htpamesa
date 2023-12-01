e5311c6ac3428edbec1e8c3acb46f59943d454e7
cruz006 <vans5364@gmail.com>
2023-12-01 11:44:29 -0800

    change title when switching tabs

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ $('.navTrigger').click(function () {
     $("#mainListDiv").fadeIn();
 
 });
+
+$(function() {
+    var message = "come back .(._. ).";
+    var original;
+  
+    $(window).focus(function() {
+      if (original) {
+        document.title = original;
+      }
+    }).blur(function() {
+      var title = $('title').text();
+      if (title != message) {
+        original = title;
+      }
+      document.title = message;
+    });
+  });
+  
+  
