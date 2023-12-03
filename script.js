3eeb469cdd2baa91e82fa35ad75b275768f4627d
cruz006 <vans5364@gmail.com>
2023-12-02 22:32:47 -0800

    do content and basic css first, make interactive later

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 $('.navTrigger').click(function () {
-    $(this).toggleClass('active');
-    console.log("Clicked menu");
-    $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
+  $(this).toggleClass('active');
+  console.log("Clicked menu");
+  $("#mainListDiv").toggleClass("show_list");
+  $("#mainListDiv").fadeIn();
 
 });
 
 $(function() {
-    var message = "come back .(._. ).";
-    var original;
-  
-    $(window).focus(function() {
-      if (original) {
-        document.title = original;
-      }
-    }).blur(function() {
-      var title = $('title').text();
-      if (title != message) {
-        original = title;
-      }
-      document.title = message;
-    });
+  var message = "come back .(._. ).";
+  var original;
+
+  $(window).focus(function() {
+    if (original) {
+      document.title = original;
+    }
+  }).blur(function() {
+    var title = $('title').text();
+    if (title != message) {
+      original = title;
+    }
+    document.title = message;
   });
-  
-  
+});
